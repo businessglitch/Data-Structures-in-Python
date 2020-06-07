bd45e18f68cc386b1ec706a2eddb834207d456f8
Fahad Hayat <fahadhayat@outlook.com>
2020-06-06 20:17:05 -0400

    Linked List with tests

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -1,8 +1,12 @@
-lass LinkedList:
+class LinkedList:
     class Node:
         def __init__(self, data):
             self.data = data
             self.next = None
+        
+        def toString(self):
+            return str(self.data)
+
         
     def __init__(self):
         self.__length = 0
@@ -13,54 +17,127 @@ lass LinkedList:
         return self.__length
     
     def peek(self):
-        return self.__head.data
-    
+        if self.__head: 
+            return self.__head.data
+        else: 
+           return None
+         
     # Clears out the list in O(n) time
     def clear(self):
-        trav = self.__head
+        if self.isEmpty():
+            return True
+        else:
 
-        while(trav.next is not None):
-            next = trav.next
-            trav.data = None
-            trav.next = None
-            trav = next
-        
-        self.__head = None
-        self.__length = 0
+            while self.__head.next is not None:
+                next = self.__head.next
+                self.__head.data = None
+                self.__head.next = None
+                self.__head = next
+
+            self.__head.data = None
+            self.__head = None
+            self.__length = 0
+
+            return True
     
     # Adds to the back of the list FIFO O(n)
-    def add(self, item:object):
+    def add(self, item: object):
         # Assign as head if list is empty
-        if (self.getSize() == 0):
+        if self.getSize() == 0:
            self. __head = self.Node(item)
         else: 
             trav = self.__head
-            while(trav.next is not None):
+            while trav.next is not None:
                 trav = trav.next
             trav.next = self.Node(item)
 
         self.__length += 1
 
     # Remove item at index and return data. O(n)
-    def remove(self, index):
+    def removeAt(self, index: int):
+       
         if index >= self.getSize() or index < 0: raise Exception ('Index:{} is out of bound'.format(index))
 
         i = 0
         prev = None
         trav = self.__head
 
-        while(i is not index):
+        while i is not index:
             prev = trav
             trav = trav.next
+            i += 1
 
+        next = trav.next
         data = trav.data
         trav.data = None
+        trav.next = None
+        trav = None
 
-        if prev is None:
-            self.__head = None
+        if prev is not None:
+            prev.next = next
         else:
-            prev.next = trav.next
+            self.__head = next
+      
 
         self.__length -= 1
 
         return data
+
+    # Remove head and return data
+    def pop(self):
+        if self.isEmpty(): raise Exception ('Cannot pop from an empty list')
+
+        next = self.__head.next
+        data = self.__head.data
+
+        self.__head.data = None
+        self.__head.next = None
+        self.__head = next
+        self.__length -= 1
+        return data
+    
+    def get(self, index: int):
+        if index >= self.getSize() or index < 0: raise Exception ('Index:{} is out of bound'.format(index))
+
+        i = 0
+        trav = self.__head
+        while i is not index:
+            trav = trav.next
+            i += 1
+        
+        return trav.data
+    
+    def indexOf(self, item: object):
+        if self.isEmpty(): return -1
+
+        i = 0
+        trav= self.__head
+        while trav is not None:
+            if trav.data == item:
+                return i
+            trav = trav.next
+            i += 1
+
+        return -1
+
+    def contains(self, item: object):
+        return self.indexOf(item) is not -1
+
+    def isEmpty(self):
+        return self.getSize() == 0
+
+    def toString(self):
+        if self.isEmpty():
+            return 'None'
+        
+        trav = self.__head
+        string = trav.toString()
+        while trav.next is not None:
+            trav = trav.next
+            string += '-->'
+            string += trav.toString()
+
+        string += '-->'
+        string += 'None'
+
+        return string
--- linked_list/test_linked_list.py
+++ linked_list/test_linked_list.py
@@ -2,4 +2,106 @@ import unittest
 import linked_list
 
 class TestLinkedList(unittest.TestCase):
-    
+
+    def test_getSize(self):
+        ll = linked_list.LinkedList()
+        self.assertEqual(ll.getSize(), 0)
+        ll.add('a') 
+        ll.add('b') 
+        
+        self.assertEqual(ll.getSize(), 2)
+
+    def test_peek(self):
+        ll = linked_list.LinkedList()
+        self.assertEqual(ll.peek(), None)
+        ll.add('a') 
+        ll.add('b') 
+        
+        self.assertEqual(ll.peek(), 'a')
+
+    def test_clear(self):
+        ll = linked_list.LinkedList()
+        self.assertEqual(ll.clear(), True)
+        ll.add('a') 
+        ll.add('b') 
+        self.assertEqual(ll.clear(), True)
+
+    def test_removeAt(self):
+        ll = linked_list.LinkedList()
+            # Test out of bound
+        with self.assertRaises(Exception):
+            ll.removeAt(0)
+        ll.add('a') 
+        ll.add('b') 
+        ll.add('c')
+
+        self.assertEqual(ll.removeAt(1), 'b')
+        self.assertEqual(ll.removeAt(0), 'a')
+        self.assertEqual(ll.removeAt(0), 'c')
+
+    def test_pop(self):
+        ll = linked_list.LinkedList()
+        # Test out of bound
+        with self.assertRaises(Exception):
+            ll.pop()
+        ll.add('b') 
+        ll.add('b') 
+        ll.add('c')
+        self.assertEqual(ll.pop(), 'b')
+        self.assertEqual(ll.pop(), 'b')
+        self.assertEqual(ll.pop(), 'c')
+
+    def test_get(self):
+        ll = linked_list.LinkedList()
+        with self.assertRaises(Exception):
+            ll.get(0)
+
+        ll.add('b') 
+        ll.add('a') 
+        ll.add('c')
+
+        self.assertEqual(ll.get(2), 'c')
+        self.assertEqual(ll.get(1), 'a')
+        self.assertEqual(ll.get(0), 'b')
+
+    def test_indexOf(self):
+        ll = linked_list.LinkedList()
+        self.assertEqual(ll.indexOf('a'), -1)
+        ll.add('b') 
+        ll.add('a') 
+        ll.add('c')
+
+        self.assertEqual(ll.indexOf('a'), 1)
+        self.assertEqual(ll.indexOf('b'), 0)
+        self.assertEqual(ll.indexOf('c'), 2)
+
+    def test_contains(self):
+        ll = linked_list.LinkedList()
+        self.assertEqual(ll.contains('a'), False)
+        ll.add('b') 
+        ll.add('a') 
+        self.assertEqual(ll.contains('b'), True)
+        self.assertEqual(ll.contains('b'), True)
+        self.assertEqual(ll.contains('c'), False)
+
+
+    def test_isEmpty(self):
+        ll = linked_list.LinkedList()
+        self.assertEqual(ll.isEmpty(), True)
+        ll.add('b') 
+        ll.add('a') 
+        self.assertEqual(ll.isEmpty(), False)
+
+    def test_toString(self):
+        ll = linked_list.LinkedList()
+        self.assertEqual(ll.toString(), 'None')
+        ll.add('b') 
+        ll.add('a') 
+        self.assertEqual(ll.toString(), 'b-->a-->None')
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()  
